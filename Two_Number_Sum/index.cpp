520db3a592d4ce3885416ee07f879728176571c6
jeel narola <narolajeel29@gmail.com>
2024-02-01 20:41:58 +0530

    C++ Start

---
+++ Two_Number_Sum/index.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int a,b;
+
+    cout << "Enter A Number Of A :-->"; 
+    cin >> a;
+
+    cout << "Enter A Number Of B :-->";
+    cin >>b;
+
+    cout << "Sum Of A and B :-->" <<a+b;
+
+    return 0;
+}
